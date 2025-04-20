d0cfa04544f938b677048ca38be9c62e96326f56
belenpor <angelin7713@gmail.com>
2025-04-20 02:50:14 -0300

    resuelto

--- JS/ejercicio3.js
+++
@@ -1,8 +0,0 @@
-
-
-//Declarar variables 
-//darle  valor a las variables
-let  radio = "7,5";
-radio = parseFloat(radio.replace(",", ".")); 
-let area=Math.PI*Math.pow(radio,2);
-console.log ("el area del circulo es: " +area.toFixed(2)+ " cm2");
