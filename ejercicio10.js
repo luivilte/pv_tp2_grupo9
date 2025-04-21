704cde0374df7eecd1a91c895035e1e0dd43affc
ivan1julio <julioivanjulio34@gmail.com>
2025-04-20 23:40:50 -0300

    subo

---
+++ ejercicio10.js
@@ -0,0 +1,8 @@
+//Solicitar al usuario que ingrese tres números enteros, luego mostrar como resultado 
+//el promedio de los tres. El promedio de tres números se calcula sumando los tres 
+//números y dividiendo entre 3.  
+const numero1 = parseInt(prompt("Ingrese el primer número entero:")); 
+const numero2 = parseInt(prompt("Ingrese el segundo número entero:")); 
+const numero3 = parseInt(prompt("Ingrese el tercer número entero:")); 
+const promedio = (numero1 + numero2 + numero3) / 3; 
+console.log("El promedio de"+ {numero1}+"," +{numero2}+ " y "+{numero3} +"es:" +{promedio}); 
