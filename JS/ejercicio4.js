f328ce802023f9407bc8c347ca82afc15c6300bb
ValerianoHJ <valerianohernanj@gmail.com>
2025-04-21 12:06:22 -0300

    ejercicio4

---
+++ JS/ejercicio4.js
@@ -0,0 +1,8 @@
+//Declarar una variable y almacenar en ella un valor numérico que representa el lado de 
+//un cuadrado. Calcular el área y el perímetro del cuadrado y mostrar los resultados por 
+//consola.
+
+let lado = 8;
+let area = Math.pow(lado,2);
+let perímetro= lado+lado+lado+lado;
+console.log("El area del cuadrado es: "+area+" y el perimetro: "+perímetro);
