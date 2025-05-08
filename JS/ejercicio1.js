c7a1f1569e4c11d4454c82e89c741958275c3c85
luivilte <vilteluis665@gmail.com>
2025-05-07 22:38:38 -0300

    Ejercicio1

---
+++ JS/ejercicio1.js
@@ -0,0 +1,8 @@
+/* 1.Crear una variable denominada integrantes y asignarle la cadena de texto con los 
+nombres de los integrantes del grupo. 
+Mostrar por consola el siguiente texto:  
+Somos (el valor de la variable) y estamos aprendiendo javascript.  */
+
+let integrantes = "Belén, María, Iván, Hernán y Luis";
+
+console.log ("Somos " + integrantes + " y estamos aprendiendo Javascript.");
